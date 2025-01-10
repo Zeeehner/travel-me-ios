487253019a31d402701b39c48e1f69e2982c875e
Noah Ra <rathaj9@gmail.com>
2025-01-10 13:25:14 +0100

    created swiftUI file

---
+++ TravelMe/TravelMe/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TravelMe
+//
+//  Created by Noah Ra on 10.01.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
